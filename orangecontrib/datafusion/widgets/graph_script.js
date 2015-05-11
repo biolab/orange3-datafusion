b0b362f5abc741a387b233133b8b4e9a06a7fb79
Kernc <kerncece@gmail.com>
2015-05-11 20:53:56 +0200

    OWFusionGraph: WebView communicates with backend Qt, also listview

--- orangecontrib/datafusion/widgets/graph_script.js
+++ orangecontrib/datafusion/widgets/graph_script.js
@@ -1,5 +1,9 @@
 
-var svg = document.getElementsByTagName('svg')[0];
+$ = document.querySelectorAll.bind(document);
+NodeList.prototype.forEach = Array.prototype.forEach;
+
+
+var svg = $('svg')[0];
 
 // Handle mousewheel zooming
 curZoom = 1;
@@ -10,7 +14,6 @@ function changeZoom(element, delta) {
     curZoom = clip(curZoom + delta, .5, 3);
     element.style.zoom = curZoom;
 }
-
 window.addEventListener('mousewheel', function(event) {
     // wheelDeltaY magic from https://developer.mozilla.org/en-US/docs/Web/Events/mousewheel
     var dy = event['deltaY'] / 20 || event['wheelDeltaY'] / 1200;
@@ -19,3 +22,30 @@ window.addEventListener('mousewheel', function(event) {
         dy = -dy;  // Apple is inverted
     changeZoom(svg, dy);
 });
+
+// Remove graph tooltip
+// See: http://graphviz.org/content/tooltip-madness
+//      http://www.graphviz.org/mantisbt/view.php?id=1491
+$('title').forEach(function(title) {
+    title.parentNode.removeChild(title);
+});
+
+
+// Highlight nodes when clicked
+var elements = $('.node, .edge')
+elements.forEach(function(elem) {
+    elem.addEventListener('click', function(event) {
+        elements.forEach(function(elem) {
+            elem.classList.remove('selected');
+        });
+        elem.classList.add('selected');
+        // Send the selection via pybridge for further processing
+        window.pybridge.graph_element_selected(elem.id);
+        //~ alert(window.pybridge);
+    });
+});
+function highlight(selector) {
+    $(selector).forEach(function(elem) {
+        elem.classList.add('selected');
+    });
+}
--- orangecontrib/datafusion/widgets/owfusiongraph.py
+++ orangecontrib/datafusion/widgets/owfusiongraph.py
@@ -11,39 +11,107 @@ JAVASCRIPT_FILE = path.join(path.dirname(__file__), 'graph_script.js')
 assert path.exists(CSS_FILE)
 JAVASCRIPT_CONTENT = open(JAVASCRIPT_FILE).read()
 
-def path2url(path):
-    from urllib.parse import urljoin
-    from urllib.request import pathname2url
-    return urljoin('file:', pathname2url(path))
+import re
 
 
 class OWFusionGraph(widget.OWWidget):
     name = "Fusion Graph"
     icon = "icons/fusion-graph.svg"
-    inputs = [("Relation", fusion.Relation, "add_relation", widget.Multiple)]
+    inputs = [("fusion.Relation", fusion.Relation, "add_relation", widget.Multiple)]
+    outputs = [("fusion.Relation", fusion.Relation)]
 
-    n_object_types = n_relations = 0
+    # Signal emitted when a node in the SVG is selected, carrying its name
+    graph_element_selected = QtCore.pyqtSignal(str)
 
     def __init__(self):
         super().__init__()
+        self.n_object_types = 0
+        self.n_relations = 0
+        self.graph_element_selected.connect(self.on_graph_element_selected)
         self.graph = fusion.FusionGraph()
         self.webview = QtWebKit.QWebView(self.mainArea)
-        self.webview.settings().setUserStyleSheetUrl(QtCore.QUrl(path2url(CSS_FILE)))
-        self.mainArea.layout().addWidget(self.webview)
+        settings = self.webview.settings()
+        def _path2url(path):
+            from urllib.parse import urljoin
+            from urllib.request import pathname2url
+            return urljoin('file:', pathname2url(path))
+        settings.setUserStyleSheetUrl(QtCore.QUrl(_path2url(CSS_FILE)))
+        if __debug__:  # TODO
+            settings.setAttribute(settings.DeveloperExtrasEnabled, True)
+        else:
+            self.webview.setContextMenuPolicy(QtCore.Qt.NoContextMenu)
         self._create_layout()
 
+    @QtCore.pyqtSlot(str)
+    def on_graph_element_selected(self, element_id):
+        """Handle self.graph_element_selected signal, and highlight also:
+           * if edge was selected, the two related nodes,
+           * if node was selected, all its edges.
+           Additionally, update the info box.
+        """
+        selected_is_edge = element_id.startswith('edge ')
+        assert element_id.startswith('edge ') or element_id.startswith('node ')
+        node_names = re.findall('`([^`]+)`', element_id)
+        nodes = [self.graph.get_object_type(name) for name in node_names]
+        assert len(nodes) == 2 if selected_is_edge else len(nodes) == 1
+        # CSS selector query for selection-relevant nodes
+        selector = ','.join('[id^="node "][id*="`%s`"]' % n.name for n in nodes)
+        # If a node was selected, include the edges that connect to it
+        if not selected_is_edge:
+            selector += ',[id^="edge "][id*="`%s`"]' % nodes[0].name
+        # Highlight these additional elements
+        self.evalJS("highlight('%s');" % selector)
+        # Update the control listview table
+        if selected_is_edge:
+            selected_relations = set(self.graph.get_relations(*nodes))
+        else:
+            selected_relations = (set(self.graph.in_relations(nodes[0])) |
+                                  set(self.graph.out_relations(nodes[0])))
+        self.listview.show_only(selected_relations)
+
     def _create_layout(self):
-        info = gui.widgetBox(self.controlArea, "Info")
+        self.mainArea.layout().addWidget(self.webview)
+        info = gui.widgetBox(self.controlArea, 'Info')
         gui.label(info, self, '%(n_object_types)d object types')
         gui.label(info, self, '%(n_relations)d relations')
+        # Table view of relation details
+        info = gui.widgetBox(self.controlArea, 'Relations')
+        class OurListWidget(QtGui.QListWidget):
+            def __init__(self, parent):
+                super().__init__(parent)
+                self.setHorizontalScrollMode(self.ScrollPerPixel)
+                self.setSelectionMode(self.SingleSelection)
+                self.setAlternatingRowColors(True)
+            def new_item_from(self, relation):
+                name = '%6d %s %s %d %s' % (relation.data.shape[0],
+                                            relation.row_type.name,
+                                            relation.name or '→',
+                                            relation.data.shape[1],
+                                            relation.col_type.name)
+                item = QtGui.QListWidgetItem(name, self)
+                item.setData(QtCore.Qt.UserRole, relation)
+                self.addItem(item)
+            def show_only(self, shown):
+                for i in range(self.count()):
+                    item = self.item(i)
+                    item.setHidden(item.data(QtCore.Qt.UserRole) not in shown)
+            def currentItemChanged(_, current, previous):
+                relation = current.getData(QtCore.Qt.UserRole)
+                self.send(self.outputs[0][0], relation)
+        self.listview = OurListWidget(info)
+        info.layout().addWidget(self.listview)
         self.controlArea.layout().addStretch(1)
 
     def add_relation(self, relation):
         self.graph.add_relation(relation)
         self.repaint(self.graph)
+        self.listview.new_item_from(relation)
         # this ensures gui.label-s get updated
         self.n_object_types = self.graph.n_object_types
         self.n_relations = self.graph.n_relations
+
+    def evalJS(self, javascript):
+        self.webview.page().mainFrame().evaluateJavaScript(javascript)
 
     def repaint(self, graph):
         stream = BytesIO()
@@ -51,7 +119,9 @@ class OWFusionGraph(widget.OWWidget):
         stream.seek(0)
         stream = QtCore.QByteArray(stream.read())
         self.webview.setContent(stream, 'image/svg+xml')
-        self.webview.page().mainFrame().evaluateJavaScript(JAVASCRIPT_CONTENT)
+        webframe = self.webview.page().mainFrame()
+        webframe.addToJavaScriptWindowObject('pybridge', self)
+        webframe.evaluateJavaScript(JAVASCRIPT_CONTENT)
         super().repaint()
 
 
