a6b07334a8cb5cda19da3d4deb0cea97e2b3bea7
Kernc <kerncece@gmail.com>
2015-10-05 13:33:09 +0200

    OWChaining: use QGraphicsView instead of Graphviz

--- orangecontrib/datafusion/widgets/owchaining.py
+++ orangecontrib/datafusion/widgets/owchaining.py
@@ -5,7 +5,7 @@ from Orange.widgets import widget, gui, settings
 
 from skfusion import fusion
 from orangecontrib.datafusion.models import Relation, FittedFusionGraph
-from orangecontrib.datafusion.widgets import owlatentfactors
+from orangecontrib.datafusion.widgets.graphview import GraphView, Edge
 from orangecontrib.datafusion.widgets.owfusiongraph import rel_cols, bold_item
 
 
@@ -13,7 +13,20 @@ class Output:
     RELATION = 'Relation'
 
 
-class OWChaining(owlatentfactors.OWLatentFactors):
+class ChainingGraphView(GraphView):
+    def __init__(self, parent):
+        super().__init__(parent)
+        self._parent = parent
+
+    def nodeClicked(self, node):
+        self._parent.on_graph_element_selected(node)
+
+    def itemClicked(self, item):
+        if isinstance(item, Edge): return
+        super().itemClicked(item)
+
+
+class OWChaining(widget.OWWidget):
     name = "Chaining"
     description = "Profile objects of one type in the feature space of " \
                   "another type through chaining of latent factors."
@@ -26,22 +39,25 @@ class OWChaining(owlatentfactors.OWLatentFactors):
 
     def __init__(self):
         super().__init__()
-        self.in_selection_mode = False
+        self.graphview = ChainingGraphView(self)
+        self.startNode = None
+        self.endNode = None
+        self._create_layout()
 
     def _create_layout(self):
         self.controlArea = self
         box = gui.widgetBox(self.controlArea, margin=7)
-        box.layout().addWidget(self.webview)
+        box.layout().addWidget(self.graphview)
         box = gui.widgetBox(self.controlArea, 'Latent chains')
         self.table = gui.TableWidget(box, select_rows=True)
-        self.table.setColumnFilter(bold_item, lambda col: col % 2)
+        self.table.setColumnFilter(bold_item, list(range(1, 100, 2)))
 
         def selectionChanged(selected, _):
             if not selected:
                 data = None
             else:
                 chain = self.table.rowData(selected[0].top())
-                self.webview.evalJS('dehighlight(ELEMENTS);')
+                self.graphview.clearSelection()
                 self._highlight_relations(chain)
                 data = self.fuser.compute_chain(chain, self.pref_complete)
             self.send(Output.RELATION, data)
@@ -63,23 +79,24 @@ class OWChaining(owlatentfactors.OWLatentFactors):
         self.controlArea.layout().addStretch(1)
 
     def _highlight_relations(self, relations):
-        escape = lambda s: s.replace(' ', r'\\ ')
         selectors = set()
         for rel in relations:
-            row_name, col_name = escape(rel.row_type.name), escape(rel.col_type.name)
-            selectors.add('.node[id*={}]'.format(row_name))
-            selectors.add('.node[id*={}]'.format(col_name))
-            selectors.add('.edge[id*={}][id*={}]'.format(row_name, col_name))
-        self.webview.evalJS('highlight("{}");'.format(','.join(selectors)))
+            row_name, col_name = rel.row_type.name, rel.col_type.name
+            node1, node2 = self.graphview.nodes[row_name], self.graphview.nodes[col_name]
+            node1.selected = True
+            node2.selected = True
+            for edge in node1.edges:
+                if node2 in edge:
+                    edge.selected = True
 
     def _populate_table(self, chains=[]):
         self.table.clear()
         self.send(Output.RELATION, None)
         for chain in chains:
-            columns = [str(self.selected_start)]
+            columns = [str(self.startNode.name)]
             for rel in chain:
                 columns += rel_cols(rel)[1:]
-            assert columns[-1] == str(self.selected_end)
+            assert columns[-1] == str(self.endNode.name)
             shape = (chain[0]. data.shape[0],
                      chain[-1].data.shape[1] if self.pref_complete else chain[-1].col_type.rank)
             self.table.addRow(['{}×{}'.format(*shape)] + columns, data=chain)
@@ -88,25 +105,17 @@ class OWChaining(owlatentfactors.OWLatentFactors):
     def on_fuser_change(self, fuser):
         self.fuser = fuser
         self._populate_table()
-        self.repaint()
+        self.graphview.fromFusionFit(fuser)
 
-    def on_graph_element_selected(self, element_id):
-        if not element_id:
-            self.in_selection_mode = False
-            return self._populate_table()
-        nodes = self.fuser.get_selected_nodes(element_id)
-        selected_is_edge = len(nodes) > 1
-        if selected_is_edge:
-            self.webview.evalJS('dehighlight(ELEMENTS);')
-            self.in_selection_mode = False
-            return self._populate_table()
-        if not self.in_selection_mode:
-            self.selected_start, self.selected_end = nodes[0], None
-            self.in_selection_mode = True
+    def on_graph_element_selected(self, node):
+        in_selection_mode = self.startNode and not self.endNode
+        if not in_selection_mode:
+            self.graphview.clearSelection()
+            self.startNode, self.endNode = node, None
         else:
-            self.selected_end = nodes[0]
-            self.in_selection_mode = False
-        if not (self.selected_start and self.selected_end):
+            self.endNode = node
+        node.selected = True
+        if not (self.startNode and self.endNode):
             return
 
         def _get_chains(ot1, ot2):
@@ -128,7 +137,8 @@ class OWChaining(owlatentfactors.OWLatentFactors):
                     paths.append((rel.col_type, path + [rel]))
             results.sort(key=len)
             return results
-        chains = _get_chains(self.selected_start, self.selected_end)
+
+        chains = _get_chains(*self.fuser.get_selected_nodes([self.startNode.name, self.endNode.name]))
 
         # Populate the listview
         self.chains = chains
@@ -136,7 +146,7 @@ class OWChaining(owlatentfactors.OWLatentFactors):
 
         # If no chains lead from start to end, reinterpret end as start
         if not chains:
-            self.on_graph_element_selected(element_id)
+            return self.graphview.nodeClicked(node)
 
 
 def main():
